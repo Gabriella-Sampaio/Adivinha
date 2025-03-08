ded193d468c106f4aab2bd8567dcbd9280a1749c
Gabriella Sampaio <dixxgabriellasam@gmail.com>
2025-03-08 12:52:51 -0300

    Término

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("O que é, o que é..."); Thread.Sleep(1500);
+Console.WriteLine("... tem no meio do coração."); Thread.Sleep(5000);
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"
++---------------+
+|  A letra ""a""  |
++---------------+ ");Thread.Sleep(1000);
+Console.ResetColor();
